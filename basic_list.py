4c9e3d1c21b6df1db2c7ca3f565e5892c5a4317d
francois <fsissoko007@yahoo.com>
2015-01-11 23:06:30 -0500

    more list comprehenion

--- basic_list.py
+++ basic_list.py
@@ -16,7 +16,7 @@ list('cat')
 
 #converts a tuple to a list
 a_tuple = ('ready', 'fire', 'aim')
-tempList=list(a_tuple)
+tempList= list(a_tuple)
 print(tempList)
 
 #list can contain list you can mutate them but you can change the list itess 
@@ -29,3 +29,31 @@ all_birds = [small_birds, extinct_birds, 'macaw', carol_birds]
 
 print(all_birds[1])
 print(all_birds[1][0])
+
+#get length by using len() 
+print("length of all birds:", len(all_birds[1]))
+#assign with =, Copy with copy()
+a = [1,2,3]
+print("a:", a)
+b = a
+print("b:", b)
+a[0] = 'surprise'
+print("a modified:", a)
+#So what is b modified 
+print("b modified:", b)
+#a and b now point and pretty much are the same thing 
+b[0] = 'I hate suprises'
+print("a modified again:", a)
+print("b modified again:", b)
+
+#You can copy the values of a list to an independent, 
+#fresh list by using any of these methods
+a = [1,2.3]
+b = a.copy() #copy method
+c = list(a) #list copy with list creation method
+d = a[:] #slice 
+a[0] = 'integer list are boring'
+print("a:", a, "b:", b, "c:", c, "d:", d)
+
+
+
