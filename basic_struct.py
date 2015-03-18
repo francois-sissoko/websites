f66469013369b5b302e5da39c25c92a6cdbe6c02
francois <fsissoko007@yahoo.com>
2015-03-18 08:50:58 -0400

    Adding cancel with break

--- basic_struct.py
+++ basic_struct.py
@@ -44,3 +44,19 @@ while count <= 5:
 	print(count)
 	count+= 1 
 
+#Cancel with break
+
+#If you want to loop until something occurs, but you're 
+#not sure when that might happen, you can use an 
+#infinite loop with a break statement.
+
+#Example
+#This time we'll read a line of input from the 
+#keyboard via Python's input() function and then
+#print with the first letter capitalize
+
+while True:
+	stuff = input("String to capitalize[type q to quit]: ")
+	if stuff == "q":
+		break
+	print(stuff.capitalize())
