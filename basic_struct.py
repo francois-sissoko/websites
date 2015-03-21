335ab93ee8e51806ae9f1be86b92cf5dfae72dd4
francois <fsissoko007@yahoo.com>
2015-03-21 09:22:05 -0400

    Iterate with for

--- basic_struct.py
+++ basic_struct.py
@@ -69,7 +69,7 @@ while True:
 #Example
 #Let's read an integer, print the square if it's
 #odd, and skip it if it's even. 
-
+print("squares an odd number only using continue:")
 while True:
 	value = input("Integer, please [q to quit]: ")
 	if value == 'q': 
@@ -99,3 +99,61 @@ while position < len(numbers):
 	position += 1
 else: #break not called
 	print("No even number found")
+	
+#Iterate with for
+
+#Python make frequent use of iterators. 
+#Make it possible to traverse data structures 
+#without knowing how large they are or how they
+#are implemented. You can even iterate over data 
+#that is created on the fly, allowing processing of
+#data streams that would otherwise not fit in 
+#memory all at once
+
+rabbits = ['Flopsy', 'Mopsy', 'Cottontail', 'Peter']
+current = 0
+while current < len(rabbits):
+	print(rabbits[current])
+	current += 1
+	
+#Better pythonic way
+for rabbit in rabbits:
+	print(rabbit)
+	
+#String iteration produces a character at a time
+
+word = 'cat'
+print("Iterate over string 'cat':")
+for letter in word:
+	print(letter)
+	
+#Iterating over a dictionary(or its keys() function)
+#returns the keys
+
+accusation = {'room': 'ballroom', 'weapon': 'lead pipe',
+'person':'Col. Mustard'}
+print("iterate over dictionary returns keys:")
+for card in accusation: #for card in accusation.keys():
+	print(card)
+
+#To iterate over the values rather than the keys,
+#You use the dictionary's values() function
+
+print("iterate over dictionary return values:")
+for value in accusation.values():
+	print(value)
+	
+#To return both the key and value in a tuple, tou can 
+#use the items() function
+
+print("iterate over dictionary returns key and value:")
+for item in accusation.items():
+	print(item)
+	
+#Remember that you can assign to a tuple in one step.
+#For each tuple returned by items(), assign the first
+#value(the key) to card and the second (the value)
+#to contents
+
+for card, contents in accusation.items():
+	print('Card',card, 'has the contents',contents)
